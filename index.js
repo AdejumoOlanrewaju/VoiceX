360a0c8a607531ba937ef3e76678d16c2aa4a8c5
AdejumoOlanrewaju <olanrewajuadejumo56@gmail.com>
2023-11-28 22:47:43 -0800

    second commit

--- index.js
+++ index.js
@@ -3,13 +3,118 @@ let taskList = document.querySelector(".task-list")
 let output = document.querySelector(".output")
 let action = document.querySelector(".action")
 let allowText = document.querySelector(".allow")
-let taskText = document.querySelectorAll(".task-text")
+let taskText;
+let btn;
+let editLink;
 
 let retrievedStore = JSON.parse(localStorage.getItem("store"))
 if(retrievedStore){
     store = retrievedStore
+    console.log(store)
     renderLists(store)
 }
+
+function renderLists(arr){
+    let list = ""
+    arr.map(text => {
+        list += `<li class = "task">
+                    <p class = "task-text">${text}</p>
+                    <div class = "task-btn-container">
+                        <button class = "task-btn">Done</button>
+
+                        <a class = "edit-link">Edit</a>
+                    </div>
+                 </li>`
+    })
+    taskList.innerHTML = list
+
+    let taskBtn = document.querySelectorAll(".task-btn")
+    taskText = document.querySelectorAll(".task-text")
+    let editLink = document.querySelectorAll(".edit-link")
+    
+    taskBtnFunc(taskBtn)
+    editText(editLink)
+    saveEdit(taskText)
+
+
+}
+ function editText(elem){
+     for(let link of elem){
+     
+         link.addEventListener("click", (event) => {
+             event.preventDefault()
+             let linkContainer = link.parentElement
+             let parent = linkContainer.parentElement
+             let text = parent.querySelector("p")
+             text.contentEditable = true
+             // console.log(text)
+             console.log("click")
+         })
+     }
+     
+    //  console.log(editLink)
+ }
+function saveEdit(elem){
+    window.addEventListener("click", function(event){
+        if(!event.target.closest(".edit-link") && !event.target.closest(".task-btn")){
+            console.log("we are there")
+            for(let i = 0; i < elem.length; i++){
+                let text = elem[i]
+                store[i] = text.textContent
+                // console.log(store[i])
+                // console.log(text)
+            } 
+            localStorage.setItem("store", JSON.stringify(store))
+        }
+        // console.log(store)
+    })
+}
+
+
+let itemLen;
+function taskBtnFunc(elem){
+    for(let [index, btn] of elem.entries()){
+        // console.log(index + ": " + btn)
+        // console.log(btn)
+        btn.addEventListener("click", function () {
+            let btnContainer = btn.parentElement
+            let parent = btnContainer.parentElement
+            let text = parent.querySelector("p")
+            console.log(text.textContent)
+            text.style.textDecoration = "line-through"
+         
+            let result = store.filter(n => n !== text.textContent)
+            store = result
+            console.log(store)
+            localStorage.setItem("store", JSON.stringify(store))
+ 
+            parent.classList.add("swipe")
+            text.classList.add("swipe")
+            btnContainer.classList.add("swipe")
+            let timeId = setTimeout(() => {
+                parent.style.display = "none"
+            }, 500)
+
+            if(store.length == 0){
+                console.log("store is empty")
+                text.textContent  = "no more task, pls click on the 'click to start' button to add new ones"
+                text.style.textDecoration = "none"
+                localStorage.clear()
+
+                parent.classList.remove("swipe")
+                text.classList.remove("swipe")
+                btnContainer.classList.remove("swipe")
+                clearTimeout(timeId)
+            }
+
+        })
+        
+    }
+}
+
+
+
+
 
 let recognition = new webkitSpeechRecognition()
 let startBtn = document.querySelector(".action-btn")
@@ -51,81 +156,4 @@ startBtn.addEventListener("click", () => {
 })
 
 
-function renderLists(arr){
-    let list = ""
-    arr.map(text => {
-        list += `<li class = "task">
-                    <p class = "task-text">${text}</p>
-                    <div class = "task-btn-container">
-                        <button class = "task-btn">Done</button>
-                    </div>
-                 </li>`
-    })
-    taskList.innerHTML = list
 
-}
-// for(let btn of taskBtn){
-    
-// }
-let taskBtn;
-taskBtn = document.querySelectorAll(".task-btn")
-    for(let btn of taskBtn){
-        btn.addEventListener("click", () => {
-            let btnContainer = btn.parentElement
-            let parent = btnContainer.parentElement
-            let text = parent.querySelector("p")
-            console.log(text.textContent)
-            text.style.textDecoration = "line-through"
-            // store.splice(index, 1)
-            // localStorage.setItem("store", JSON.stringify(store))
-            console.log(store)
-            // localStorage.clear()
-            // console.log(store)
-        })
-        // console.log(btn + ":" + index)
-        // console.log(btn)
-        
-}
-
-
-// if(taskBtn.length > 1){
-//     for(let btn of taskBtn){
-//         btn.addEventListener("click", () => {
-//             let btnContainer = btn.parentElement
-//             let parent = btnContainer.parentElement
-//             let text = parent.querySelector("p")
-//             console.log(text.textContent)
-//             text.style.textDecoration = "line-through"
-//             // localStorage.clear()
-//             // console.log(store)
-//             // store.splice(index, 1)
-//             // console.log(store)
-//         })
-        
-//     }
-
-// }else{
-//     taskBtn = document.querySelector(".task-btn")
-//     taskBtn.addEventListener('click', ()=> {
-//         console.log("seen")
-//     })
-// }
-
-// num = [1, 2, 4]
-// let index = num.indexOf(1)
-// num.splice(1, 1)
-
-// console.log(num.entries())
-// if (taskBtn.length > 1){
-//     for(let btn of taskBtn){
-//         btn.addEventListener("click", ()=>{
-//             console.log("cleared")
-//         })
-//     }
-// }
-// for (let [index, n] of num.entries()){
-//     console.log(n)
-//     console.log(index)
-// }
-console.log(taskBtn.length)
-console.log(taskText.length)
